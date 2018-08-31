87aeab29059ee4d1055c36f9ce30d91150df6f2c
Andrea <azaupa@gmail.com>
2018-08-31 10:21:20 +0200

    add master key

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,10 @@
 lock "~> 3.11.0"
 
 set :application, "test_webpack"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/andreazaupa/test_webpack.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', "config/master.key")
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
